77e4cfe5567126f90f3b86dede28e469053395a3
Raffi <N/A>
2019-06-24 10:34:37 +0800

    automated build

--- html/diddle/copy.php
+++ html/diddle/copy.php
@@ -4,6 +4,13 @@ print DIDDLE_ID;
 $sandbox = new DiddleSandbox (DIDDLE_ID, get_diddler());
 $new_sandbox = new DiddleSandbox (DiddleSandbox::get_new_id(), get_diddler());
 $new_sandbox->init_assets();
+
+$old_fs_stream = fopen($sandbox->file, 'r');
+$new_fs_stream = fopen($new_sandbox->file, 'w');
+stream_copy_to_stream($old_fs_stream, $new_fs_stream);
+fclose($old_fs_stream);
+fclose($new_fs_stream);
+
 header("location: /diddle/{$new_sandbox->id}");
 
 
