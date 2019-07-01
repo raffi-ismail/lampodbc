a3a92d5d02a5b921090ba634ce7f4726ebbf6f97
Raffi <N/A>
2019-07-01 20:13:42 +0800

    Fixed bug with default PORT value

--- startup.sh
+++ startup.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
 ENV_JSON=/var/www/env.json
-if [ -z ${DEFAULT_LISTEN_PORT_HTTP} ]; then PORT=80; fi;
+if [ -z ${PORT} ]
+then 
+    echo "\nENV PORT was not set, defaulting to DEFAULT_LISTEN_PORT_HTTP(${DEFAULT_LISTEN_PORT_HTTP})...\n";
+    PORT=${DEFAULT_LISTEN_PORT_HTTP}; 
+fi;
 
+echo "\nPreparing Apache envars...\n"
 printenv | jq --raw-input --slurp \
     'split("\n")[:-1] | map(split("=")) | map_values ( { (.[0]) : (.[1:]) | join("=") } ) | add' \
     > $ENV_JSON && \
@@ -14,7 +19,10 @@ env | sed "s/\(.*\)=\(.*\)/export \1='\2'/" >> /etc/apache2/envvars
 #echo "clear_env = no" >> /etc/php/7.0/fpm/pool.d/www.conf
 #env | sed "s/\(.*\)=\(.*\)/env[\1]='\2'/" >> /etc/php/7.0/fpm/pool.d/www.conf
 
+echo "\nStasting sshd...\n"
 /etc/init.d/ssh start
+
+echo "\nStarting Apache in the foreground, listening to port ${PORT}...\n"
 apache2ctl -k start -DFOREGROUND
 
 #/var/www/code-server/code-server /var/www
