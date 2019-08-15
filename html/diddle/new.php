ac066d76dd3f6a1a1777447c5d325a0c28142e90
Raffi <N/A>
2019-08-15 13:48:08 +0800

    Moved fiddle out of image

--- html/diddle/new.php
+++
@@ -1,7 +0,0 @@
-<?php
-include_once('common.php');
-$new_sandbox = new DiddleSandbox (DiddleSandbox::get_new_id(), get_diddler());
-$new_sandbox->init_assets();
-header("location: /diddle/{$new_sandbox->id}");
-
-
